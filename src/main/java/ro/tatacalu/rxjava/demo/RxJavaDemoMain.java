ce39faa736ea919acd12f108e6bc16ff2f48256b
Matei Dragu <tatacalu@users.noreply.github.com>
2017-03-16 23:40:54 +0000

    Add RxJava 2.0.7

--- src/main/java/ro/tatacalu/rxjava/demo/RxJavaDemoMain.java
+++ src/main/java/ro/tatacalu/rxjava/demo/RxJavaDemoMain.java
@@ -1,5 +1,6 @@
 package ro.tatacalu.rxjava.demo;
 
+import io.reactivex.Flowable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class RxJavaDemoMain {
     private static final Logger LOGGER = LoggerFactory.getLogger(RxJavaDemoMain.class);
 
     public  static void main(String[] args) {
-        LOGGER.info("RxJava Demo Main");
+        Flowable.just("RxJava Demo - Hello world!").subscribe(LOGGER::info);
     }
 }
