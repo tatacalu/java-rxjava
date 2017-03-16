80921080337b0f4548eacb126a7c0889d2122c0c
Matei Dragu <tatacalu@users.noreply.github.com>
2017-03-16 22:57:22 +0000

    Main class - application entry point

---
+++ src/main/java/ro/tatacalu/rxjava/demo/RxJavaDemoMain.java
@@ -0,0 +1,16 @@
+package ro.tatacalu.rxjava.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Application entry point.
+ */
+public class RxJavaDemoMain {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RxJavaDemoMain.class);
+
+    public  static void main(String[] args) {
+        LOGGER.info("RxJava Demo Main");
+    }
+}
